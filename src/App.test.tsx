be6630bf786189501945b14ca0267e15d0cf4a49
Robert Gravina <robert.gravina@gmail.com>
2023-12-11 20:29:39 +1100

    Add tests for main component.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,72 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('displays some totals', () => {
-  render(<App />);
-});
+const AppPage = {
+  vmw: async (value?: string) => {
+    const input = await screen.findByTestId("vmw") as HTMLInputElement
+    if (value) {
+      fireEvent.change(input, {target: {value}})      
+      return
+    }
+    return input.value;
+  },
+
+  avgo: async (value?: string) => {
+    const input = await screen.findByTestId("avgo") as HTMLInputElement
+    if (value) {
+      fireEvent.change(input, {target: {value}})      
+      return
+    }
+    return (await screen.findByTestId("avgo") as HTMLInputElement).value;
+  },
+
+  stock: async () => {
+    return (await screen.findByTestId("stock") as HTMLLIElement).textContent;
+  },
+
+  cash: async () => {
+    return (await screen.findByTestId("cash") as HTMLLIElement).textContent;
+  },
+
+  stockPeak: async () => {
+    return (await screen.findByTestId("stock-peak") as HTMLLIElement).textContent;
+  },
+
+  allCash: async () => {
+    return (await screen.findByTestId("all-cash") as HTMLLIElement).textContent;
+  }
+}
+
+describe("App", () => {
+  const page = AppPage;
+
+  it('displays form prefilled with defaults', async () => {
+    render(<App />);
+    expect(await page.vmw()).toBe("100")
+    expect(await page.avgo()).toBe("13")
+  });
+
+  it('stock values can be changed', async () => {
+    render(<App />);
+    await page.vmw("200")
+    await page.avgo("20")
+    expect(await page.stock()).toBe("39.68% of your VMW is AVGO now (at a conversion rate of 0.252 AVGO per VMW share).")
+  });
+
+  it('displays stock portion', async () => {
+    render(<App />);
+    expect(await page.stock()).toBe("51.59% of your VMW is AVGO now (at a conversion rate of 0.252 AVGO per VMW share).")
+  });
+
+  it('displays cash portion', async () => {
+    render(<App />);
+    expect(await page.cash()).toBe("48.41% of your VMW was converted to cash at a price of $142.00.")
+  });
+
+  it('displays other info', async () => {
+    render(<App />);
+    expect(await page.stockPeak()).toBe("Your VMW was worth $18,061.00 before conversion when it hit its peak of $180.61.")
+    expect(await page.allCash()).toBe("Your VMW would be worth $14,200.00 if you got all cash at a price of $142.00.")
+  });
+})
--- src/App.tsx
+++ src/App.tsx
@@ -37,29 +37,29 @@ function App() {
       </header>
       <main>
         <div className="form">
-        <div>You had: <input value={vmwStock} maxLength={5} onChange={(e) => setVmwStock(Number(e.target.value))}/> <span className="vmw">VMW</span>.</div>
-        <div>You have: <input value={avgoStock} maxLength={5} onChange={(e) => setAvgoStock(Number(e.target.value))}/> <span className="avgo">AVGO</span>.</div>
+        <div>You had: <input data-testid="vmw" value={vmwStock} maxLength={5} onChange={(e) => setVmwStock(Number(e.target.value))}/> <span className="vmw">VMW</span>.</div>
+        <div>You have: <input data-testid="avgo" value={avgoStock} maxLength={5} onChange={(e) => setAvgoStock(Number(e.target.value))}/> <span className="avgo">AVGO</span>.</div>
         </div>
         <div>
         <div className="stock">
           <h2>Stock Portion</h2>
           <ul>
-            <li>{avgoPercent.toFixed(2)}% of your <span className="vmw">VMW</span> is <span className="avgo">AVGO</span> now (at a conversion rate of {avgo.stockConversionRate} <span className="avgo">AVGO</span> per <span className="vmw">VMW</span> share).</li>
+            <li data-testid="stock">{avgoPercent.toFixed(2)}% of your <span className="vmw">VMW</span> is <span className="avgo">AVGO</span> now (at a conversion rate of {avgo.stockConversionRate} <span className="avgo">AVGO</span> per <span className="vmw">VMW</span> share).</li>
             <li>Your <span className="avgo">AVGO</span> is worth {usdFormatter.format(avgoValue)} at its current price of <input type="number" value={avgoStockPrice} maxLength={5} onChange={(e) => setAvgoStockPrice(Number(e.target.value))}/>.</li>
           </ul>
           </div>
           <div className="cash">
             <h2>Cash Portion</h2>
             <ul>
-              <li>{cashPercent.toFixed(2)}% of your <span className="vmw">VMW</span> was converted to cash at a price of {usdFormatter.format(vmw.priceAquired)}.</li>
+              <li data-testid="cash">{cashPercent.toFixed(2)}% of your <span className="vmw">VMW</span> was converted to cash at a price of {usdFormatter.format(vmw.priceAquired)}.</li>
               <li>{usdFormatter.format(expectedCash)} before tax cash expected.</li>
               <li>{usdFormatter.format(expectedCash * (100 - withholdingTax)/100)} after <input className="percentage" value={withholdingTax} maxLength={2} onChange={(e) => setWithholdingTax(Number(e.target.value))}/>% US tax withholding.</li>
             </ul>
             </div>
           <h2>Other Info</h2>
           <ul>
-            <li>Your <span className="vmw">VMW</span> was worth {usdFormatter.format(vmwStock * vmw.priceBeforeClose)} before conversion when it hit its peak of {usdFormatter.format(vmw.priceBeforeClose)}.</li>
-            <li>Your <span className="vmw">VMW</span> would be worth {usdFormatter.format(vmwStock * vmw.priceAquired)} if you got all cash at a price of {usdFormatter.format(vmw.priceAquired)}.</li>
+            <li data-testid="stock-peak">Your <span className="vmw">VMW</span> was worth {usdFormatter.format(vmwStock * vmw.priceBeforeClose)} before conversion when it hit its peak of {usdFormatter.format(vmw.priceBeforeClose)}.</li>
+            <li data-testid="all-cash">Your <span className="vmw">VMW</span> would be worth {usdFormatter.format(vmwStock * vmw.priceAquired)} if you got all cash at a price of {usdFormatter.format(vmw.priceAquired)}.</li>
           </ul>
         </div>
       </main>
